1f7f08e8cd7164b4f52e97556d2d5a69b464dc3f
Mariama <mariamabah@Mariamas-MacBook-Pro.local>
2023-03-03 23:39:59 -0500

    deploy

--- config/connection.js
+++ config/connection.js
@@ -10,8 +10,8 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
+  sequelize = new Sequelize("tech_blog", "root", "", {
+    host: '127.0.0.1',
     dialect: 'mysql',
     port: 3306
   });
